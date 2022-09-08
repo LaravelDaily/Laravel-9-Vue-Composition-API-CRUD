5e93b9b59f21a31810bc45bcb60205efe88d2b86
Nerijus <krekas@gmail.com>
2022-09-08 19:26:37 +0300

    List and Create company

---
+++ resources/js/components/companies/CompaniesCreate.vue
@@ -0,0 +1,70 @@
+<template>
+    <div v-if="errors">
+        <div v-for="(v, k) in errors" :key="k" class="bg-red-400 text-white rounded font-bold mb-4 shadow-lg py-2 px-4 pr-0">
+            <p v-for="error in v" :key="error" class="text-sm">
+                {{ error }}
+            </p>
+        </div>
+    </div>
+
+    <form class="space-y-6" @submit.prevent="saveCompany">
+        <div class="space-y-4 rounded-md shadow-sm">
+            <div>
+                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                <div class="mt-1">
+                    <input type="text" name="name" id="name"
+                           class="block mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                           v-model="form.name">
+                </div>
+            </div>
+
+            <div>
+                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                <div class="mt-1">
+                    <input type="text" name="email" id="email"
+                           class="block mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                           v-model="form.email">
+                </div>
+            </div>
+
+            <div>
+                <label for="address" class="block text-sm font-medium text-gray-700">Address</label>
+                <div class="mt-1">
+                    <input type="text" name="address" id="address"
+                           class="block mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                           v-model="form.address">
+                </div>
+            </div>
+
+            <div>
+                <label for="website" class="block text-sm font-medium text-gray-700">Website</label>
+                <div class="mt-1">
+                    <input type="text" name="website" id="website"
+                           class="block mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                           v-model="form.website">
+                </div>
+            </div>
+        </div>
+
+        <button type="submit"
+                class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase bg-gray-800 rounded-md border border-transparent ring-gray-300 transition duration-150 ease-in-out hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring disabled:opacity-25">
+            Create
+        </button>
+    </form>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import useCompanies from "@/composables/companies";
+
+const form = reactive({
+    'name': '',
+    'email': '',
+    'address': '',
+    'website': '',
+})
+const { errors, storeCompany } = useCompanies()
+const saveCompany = async () => {
+    await storeCompany({ ...form });
+}
+</script>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import CompaniesIndex from '@/components/companies/CompaniesIndex.vue'
+import CompaniesCreate from '@/components/companies/CompaniesCreate.vue'
 
 const routes = [
     {
         path: '/dashboard',
         name: 'companies.index',
         component: CompaniesIndex
+    },
+    {
+        path: '/companies/create',
+        name: 'companies.create',
+        component: CompaniesCreate
     }
 ]
 
