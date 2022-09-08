d744d5ff81771d4e85b1698be7e981c105b79218
Nerijus <krekas@gmail.com>
2022-09-08 19:37:01 +0300

    Company edit

--- resources/js/composables/companies.js
+++ resources/js/composables/companies.js
@@ -4,6 +4,7 @@ import { useRouter } from 'vue-router';
 
 export default function useCompanies() {
     const companies = ref([])
+    const company = ref([])
     const router = useRouter()
     const errors = ref('')
 
@@ -12,10 +13,27 @@ export default function useCompanies() {
         companies.value = response.data.data;
     }
 
+    const getCompany = async (id) => {
+        let response = await axios.get('/api/companies/' + id)
+        company.value = response.data.data;
+    }
+
     const storeCompany = async (data) => {
         errors.value = ''
         try {
             await axios.post('/api/companies/', data)
+            await router.push({name: 'companies.index'})
+        } catch (e) {
+            if (e.response.status === 422) {
+                errors.value = e.response.data.errors
+            }
+        }
+    }
+
+        const updateCompany = async (id) => {
+        errors.value = ''
+        try {
+            await axios.put('/api/companies/' + id, company.value)
             await router.push({name: 'companies.index'})
         } catch (e) {
             if (e.response.status === 422) {
@@ -31,9 +49,12 @@ export default function useCompanies() {
 
     return {
         companies,
+        company,
         errors,
         getCompanies,
+        getCompany,
         storeCompany,
+        updateCompany,
         destroyCompany
     }
 }
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import CompaniesIndex from '@/components/companies/CompaniesIndex.vue'
 import CompaniesCreate from '@/components/companies/CompaniesCreate.vue'
+import CompaniesEdit from '@/components/companies/CompaniesEdit.vue'
 
 const routes = [
     {
@@ -13,6 +14,12 @@ const routes = [
         path: '/companies/create',
         name: 'companies.create',
         component: CompaniesCreate
+    },
+    {
+        path: '/companies/:id/edit',
+        name: 'companies.edit',
+        component: CompaniesEdit,
+        props: true
     }
 ]
 
